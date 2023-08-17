36879d9fe9038b9156ae645e6b72dd486183b770
AyushGit1253 <ayush1253khadatkar@gmail.com>
2023-08-17 22:53:13 +0530

    createServer

--- createServer.js
+++ createServer.js
@@ -1,9 +1,14 @@
+
+
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Ayush');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Ayush</h1>');
+    console.log('My name is ChatGPT.');
+    res.end()
 });
 
 server.listen(4000, () => {
-    console.log('Ayush');
+    console.log('Server is running on port 4000');
 });
