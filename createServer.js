e02efc068f8678616c855bb4af248e06603f421b
AyushGit1253 <ayush1253khadatkar@gmail.com>
2023-08-17 22:33:04 +0530

    createServer

---
+++ createServer.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log('NodeJS server creation');
+});
+
+server.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
